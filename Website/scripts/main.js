822b86c3d1f918264632776ecb9d1c4664e61190
shadowannarock <raoxuejiang@Shadowannarock.local>
2023-02-19 17:16:02 +0800

    update index.html and create style.css, create script.js

---
+++ Website/scripts/main.js
@@ -0,0 +1,6 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'This is a website about London gallery!';
+
+document.querySelector("img").addEventListener("click",function(){
+		alert("Have you ever been London?");
+		});
