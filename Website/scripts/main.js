ecd7f9a327057291335e44c6a617de52fa237d78
shadowannarock <raoxuejiang@Shadowannarock.local>
2023-02-20 19:11:47 +0800

    modified some files and rename them.

--- Website/scripts/main.js
+++ Website/scripts/main.js
@@ -1,6 +1,9 @@
+
+/* modify the content of h1*/
 let myHeading = document.querySelector('h1');
 myHeading.textContent = 'This is a website about London gallery!';
 
+/**/
 document.querySelector("img").addEventListener("click",function(){
 		alert("Have you ever been London?");
 		});
